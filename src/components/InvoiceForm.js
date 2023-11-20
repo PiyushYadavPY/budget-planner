cdfe7cf20442fb913e8b9bfbc04360177a852ff8
piyushyadavpy <piyushyadav4991@gmail.com>
2023-11-20 11:59:41 +0530

    budget-planner done

---
+++ src/components/InvoiceForm.js
@@ -0,0 +1,89 @@
+import React, { useState, useReducer } from 'react';
+import InvoiceItems from './InvoiceItems';
+import { jsPDF } from 'jspdf';
+
+const initialState = {
+  invoiceNumber: '',
+  date: '',
+  billingAddress: '',
+  items: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return { ...state, items: [...state.items, action.payload] };
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        items: state.items.filter((_, index) => index !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+const InvoiceForm = () => {
+  const [formData, setFormData] = useState(initialState);
+  const [state, dispatch] = useReducer(reducer, formData);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleAddItem = (item) => {
+    dispatch({ type: 'ADD_ITEM', payload: item });
+  };
+
+  const handleRemoveItem = (index) => {
+    dispatch({ type: 'REMOVE_ITEM', payload: index });
+  };
+
+  const generatePDF = () => {
+   const doc = new jsPDF();
+    doc.text(`Invoice Number: ${state.invoiceNumber}`, 10, 10);
+    state.items.forEach((item, index) => {
+      doc.text(`${index + 1}. ${item.description} - ${item.quantity} x ${item.price}`, 10, 20 + index * 10);
+    });
+    doc.save('invoice.pdf');
+  }; 
+
+  return (
+    <div className="invoice-form mx-auto w-3/4 bg-gray-100 p-6 rounded-lg">
+      <input
+        type="text"
+        name="invoiceNumber"
+        placeholder="Invoice Number"
+        value={formData.invoiceNumber}
+        onChange={handleInputChange}
+        className="w-full border border-gray-300 rounded px-3 py-2 my-2"
+      />
+      <input
+        type="text"
+        name="date"
+        placeholder="Date"
+        value={formData.date}
+        onChange={handleInputChange}
+        className="w-full border border-gray-300 rounded px-3 py-2 my-2"
+      />
+      <textarea
+        name="billingAddress"
+        placeholder="Billing Address"
+        value={formData.billingAddress}
+        onChange={handleInputChange}
+        className="w-full border border-gray-300 rounded px-3 py-2 my-2 resize-none"
+      ></textarea>
+      <InvoiceItems
+        items={state.items}
+        onAddItem={handleAddItem}
+        onRemoveItem={handleRemoveItem}
+      />
+      <button onClick={generatePDF} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Generate PDF
+      </button>
+    </div>
+  );
+};
+
+export default InvoiceForm;
---
+++ src/components/InvoiceItems.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const InvoiceItems = ({ items, onAddItem, onRemoveItem }) => {
+  const [newItem, setNewItem] = useState({
+    description: '',
+    quantity: '',
+    price: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewItem({ ...newItem, [name]: value });
+  };
+
+  const handleAdd = () => {
+    onAddItem(newItem);
+    setNewItem({ description: '', quantity: '', price: '' });
+  };
+
+  return (
+    <div className="my-4">
+      <h3 className="text-lg font-semibold mb-2">Items</h3>
+      {items.map((item, index) => (
+        <div key={index} className="flex justify-between items-center mb-2">
+          {/* Display items */}
+          <span>{item.description}</span>
+          {/* Other item details */}
+          <button onClick={() => onRemoveItem(index)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+            Remove
+          </button>
+        </div>
+      ))}
+      {/* Form to add new items */}
+      <input
+        type="text"
+        name="description"
+        placeholder="Description"
+        value={newItem.description}
+        onChange={handleInputChange}
+        className="w-full border border-gray-300 rounded px-3 py-2 my-1"
+      />
+      <input
+        type="number"
+        name="quantity"
+        placeholder="Quantity"
+        value={newItem.quantity}
+        onChange={handleInputChange}
+        className="w-full border border-gray-300 rounded px-3 py-2 my-1"
+      />
+      <input
+        type="number"
+        name="price"
+        placeholder="Price"
+        value={newItem.price}
+        onChange={handleInputChange}
+        className="w-full border border-gray-300 rounded px-3 py-2 my-1"
+      />
+      <button onClick={handleAdd} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        Add Item
+      </button>
+    </div>
+  );
+};
+
+export default InvoiceItems;
